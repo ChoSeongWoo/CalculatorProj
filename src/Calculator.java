91712821b4ec41ffe540f0c13aada01ba6cd46b7
user <user@user171>
2016-04-05 18:53:24 +0900

    Complete sub method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,10 +7,14 @@ public class Calculator {
 	public int add(int x, int y){
 		return x+y;
 	}
+	public int sub(int x, int y){
+		return x-y;
+	}
 	public static void main(String[] args) {
 		
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(1,2));
+		System.out.println(cal.sub(1,2));
 		
 
 	}
